2782b3cfc23b51ee19f28a130dbd208a2b96396c
taichi <taichi_nakamura@jp.honda>
2022-09-09 10:44:59 +0900

    modified

--- docker_run_ridercsa.sh
+++ docker_run_ridercsa.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 docker run -it --gpus all \
-    -e XMODIFIRES \
     -e NVIDIA_DRIVER_CAPABILITIES=all \
-    -e DISPLAY=$DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
-    -v /mnt/SSD_2TB/data/cs:/mnt/SSD_2TB/data/cs \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /mnt/d/UserArea/j0135417/Documents/08_docker_mount:/mnt/ \
     --add-host hostname:127.0.0.1\
     --hostname hostname\
+    -e XMODIFIRES \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     -u nvidia\
     -w /home/nvidia\
     --privileged \
     --net host \
-    --name rider_csa_container22 \
+    --name rider_csa_container \
     taichimojamoja/ubuntu_ros_opencv
